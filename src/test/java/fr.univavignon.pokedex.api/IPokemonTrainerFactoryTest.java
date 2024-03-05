25aa6fa82dcfab5b1f6fc32de01d915050e2349c
SAADOUN Amine <mohamed-amine.saadoun@alumni.univ-avignon.fr>
2024-03-05 13:27:32 +0100

    passed test "IPokemonTrainerFactoryTest"

--- src/test/java/fr.univavignon.pokedex.api/IPokemonTrainerFactoryTest.java
+++ src/test/java/fr.univavignon.pokedex.api/IPokemonTrainerFactoryTest.java
@@ -1,5 +1,69 @@
 package fr.univavignon.pokedex.api;
 
+import org.junit.jupiter.api.BeforeAll;
+        import org.junit.jupiter.api.Test;
+        import org.junit.jupiter.api.TestInstance;
+
+        import static org.junit.Assert.assertEquals;
+        import static org.junit.Assert.assertThrows;
+        import static org.junit.jupiter.api.Assertions.assertNotNull;
+        import static org.mockito.Mockito.mock;
+        import static org.mockito.Mockito.when;
+
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
 public class IPokemonTrainerFactoryTest {
+    private IPokemonTrainerFactory pokemonTrainerFactory;
+
+    @BeforeAll
+    public void setUp(){
+        pokemonTrainerFactory = mock(IPokemonTrainerFactory.class);
+    }
+
+
+    @Test
+    public void testCreatePokemonTrainer() {
+        Team team = Team.INSTINCT;
+        String trainerName = "Ash";
+
+        IPokedexFactory pokedexFactory = mock(IPokedexFactory.class);
+        IPokedex pokedex = mock(IPokedex.class);
+        IPokemonMetadataProvider pokemonMetadataProvider = mock(IPokemonMetadataProvider.class);
+        IPokemonFactory pokemonFactory = mock(IPokemonFactory.class);
+
+        when(pokedexFactory.createPokedex(pokemonMetadataProvider, pokemonFactory)).thenReturn(pokedex);
+
+        when(pokemonTrainerFactory.createTrainer(trainerName, team, pokedexFactory))
+                .thenReturn(new PokemonTrainer(trainerName, team, pokedex));
+
+        PokemonTrainer pokemonTrainer = pokemonTrainerFactory.createTrainer(trainerName, team, pokedexFactory);
+
+        assertNotNull(pokemonTrainer);
+        assertEquals(trainerName, pokemonTrainer.getName());
+        assertEquals(team, pokemonTrainer.getTeam());
+    }
+
+
+    @Test
+    public void testCreateWithNullParams() {
+        Team team = Team.INSTINCT;
+        String trainerName = "Ash";
+
+
+        IPokedexFactory pokedexFactory = mock(IPokedexFactory.class);
+        IPokedex pokedex = mock(IPokedex.class);
+        IPokemonMetadataProvider pokemonMetadataProvider = mock(IPokemonMetadataProvider.class);
+        IPokemonFactory pokemonFactory = mock(IPokemonFactory.class);
+
+        when(pokedexFactory.createPokedex(pokemonMetadataProvider, pokemonFactory)).thenReturn(pokedex);
+
+        when(pokemonTrainerFactory.createTrainer(null, team, pokedexFactory)).thenThrow(new IllegalArgumentException());
+        when(pokemonTrainerFactory.createTrainer(trainerName, null, pokedexFactory)).thenThrow(new IllegalArgumentException());
+        when(pokemonTrainerFactory.createTrainer(trainerName, team, null)).thenThrow(new IllegalArgumentException());
+
+        assertThrows(IllegalArgumentException.class, () -> pokemonTrainerFactory.createTrainer(null, team, pokedexFactory));
+        assertThrows(IllegalArgumentException.class, () -> pokemonTrainerFactory.createTrainer(trainerName, null, pokedexFactory));
+        assertThrows(IllegalArgumentException.class, () -> pokemonTrainerFactory.createTrainer(trainerName, team, null));
+    }
+
 
 }
