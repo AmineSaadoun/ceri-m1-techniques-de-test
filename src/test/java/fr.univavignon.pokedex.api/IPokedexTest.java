6978693d7a428621fb7b113457cd49ac443e0711
SAADOUN Amine <mohamed-amine.saadoun@alumni.univ-avignon.fr>
2024-03-06 15:49:18 +0100

    failed test "class IPokedexTest"

--- src/test/java/fr.univavignon.pokedex.api/IPokedexTest.java
+++ src/test/java/fr.univavignon.pokedex.api/IPokedexTest.java
@@ -1,5 +1,56 @@
 package fr.univavignon.pokedex.api;
 
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
+
+import java.util.Arrays;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
 public class IPokedexTest {
+    private IPokedex iPokedex;
+    List<Pokemon> pokemons;
+    @BeforeAll
+    public void setUp() {
+        // mock pour l'interface IPokedex
+        this.iPokedex = mock(IPokedex.class);
+
+
+        //Une liste de pokemons
+        pokemons = Arrays.asList(
+                new Pokemon(0,
+                        "Bulbizarre", 130, 130,
+                        100, 613, 64,
+                        4000, 4, 0.91),
+
+                new Pokemon(133, "Aquali", 186,
+                        168, 260, 2729, 202
+                        , 5000, 4, 0.91),
+                new Pokemon(1,
+                        "hamid", 190, 120,
+                        434, 302, 45,
+                        2000, 4, 0.80)
+        );
+    }
+
+
+    @Test
+    public void sizeTest() {
+        when(this.iPokedex.size()).thenReturn(151);
+        assertNotEquals(151, this.iPokedex.size());
+    }
+
+
+
+
+
+
+
+
 
 }
